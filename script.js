cbb628e2a3b6a9df4cea3ebbebbf01a58a7f731b
andreas893 <eaa24andrh@students.eaaa.dk>
2025-02-05 10:51:23 +0100

    opg2

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 "use strict";
 
-const klasseliste = ["Lasse", "Andreas", "Gauti", "Astrid", "Marie", "Troels", "Malou"];
+// const klasseliste = ["Lasse", "Andreas", "Gauti", "Astrid", "Marie", "Troels", "Malou"];
 
-for (let i = 0; i < klasseliste.length; i++) {
-    console.log(`${i + 1}. ${klasseliste[i]}`);
+// for (let i = 0; i < klasseliste.length; i++) {
+//     console.log(`${i + 1}. ${klasseliste[i]}`);
+// }
+
+function message(){
+    let firstName = prompt("Indtast dit navn her:");
+    
+    alert(`Hej ${firstName}! Ha' en god dag!`);
+
 }
+
+message();
