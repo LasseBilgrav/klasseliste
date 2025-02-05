a972bd0205c9768fc11dc33286143fb8020dbadc
LasseBilgrav <eaa24lsbi@students.eaaa.dk>
2025-02-05 10:23:39 +0100

    yur

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const klasseliste = ["Lasse", "Andreas", "Gauti", "Astrid", "Marie", "Troels", "Malou"];
+
+for (let i = 0; i < klasseliste.length; i++) {
+    console.log(`${i + 1}. ${klasseliste[i]}`);
+}
