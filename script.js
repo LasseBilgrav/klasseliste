6a3af5531897daef8224307ac31d77c2626f685b
andreas893 <eaa24andrh@students.eaaa.dk>
2025-02-05 11:11:07 +0100

    update

--- script.js
+++ script.js
@@ -6,11 +6,34 @@
 //     console.log(`${i + 1}. ${klasseliste[i]}`);
 // }
 
-function message(){
-    let firstName = prompt("Indtast dit navn her:");
+// function message(){
+//     let firstName = prompt("Indtast dit navn her:");
     
-    alert(`Hej ${firstName}! Ha' en god dag!`);
+//     alert(`Hej ${firstName}! Ha' en god dag!`);
 
+// }
+
+// message();
+
+function adgang(){
+    let age = prompt("Hvor gammel er du?")
+    age = parseInt(age);
+    
+    while(condition) {
+        if(isNaN(age)){
+            alert("Indtast en gyldig alder");
+            return;
+        }
+
+        if(age < 18){
+            alert("Du er ikke gammel nok til denne hjemmeside")
+            break;
+        }
+        else if(age >= 18){
+            alert("Du er gammel nok! Velkommen til hjemmesiden")
+            break;
+        }
+    }    
 }
 
-message();
+adgang();
